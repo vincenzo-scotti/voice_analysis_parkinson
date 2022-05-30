09200c45f01dc178cf0f927382635a209a707109
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-05-30 11:29:09 +0200

    Add audio loading function output unpacking.

--- src/features/soundnet.py
+++ src/features/soundnet.py
@@ -3,7 +3,7 @@ from .utils import load_audio, pooling
 
 
 def get_soundnet_features(file_path: str, t_pooling: Optional[str] = None, tgt_len: Optional[float] = None):
-    data = load_audio(file_path, tgt_len=tgt_len)
+    data, sample_rate = load_audio(file_path, tgt_len=tgt_len)
     ...
     if pooling is not None:
         return pooling(data, t_pooling)
