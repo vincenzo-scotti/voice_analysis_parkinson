ea4c082facb1a54870cb9951794f657ca06332d9
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2022-11-02 14:38:59 +0100

    [CF] added wav2vec feature extractor

--- src/features/wav2vec.py
+++ src/features/wav2vec.py
@@ -1,17 +1,22 @@
 from typing import Optional
 import numpy as np
+import torch
+from transformers import Wav2Vec2Processor, Wav2Vec2Model,Wav2Vec2CTCTokenizer, AutoFeatureExtractor
 from .utils import load_audio, pooling, GlobalPooling
 
 wav2vec: Optional = None
+processorWav2vec: Optional = None
 
-
-def get_wav2vec(*args, **kwargs):
+def get_wav2vec():
     global wav2vec
+    global processorWav2vec
     if wav2vec is None:
-        ...
-
-    return wav2vec
-
+        wav2vec = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+    if processorWav2vec is None:
+        feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+        tokenizer = Wav2Vec2CTCTokenizer("./vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
+        processorWav2vec = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
+    return wav2vec, processorWav2vec
 
 def get_wav2vec_features(
         file_path: str,
@@ -19,11 +24,17 @@ def get_wav2vec_features(
         t_pooling: Optional[GlobalPooling] = None,
         tgt_len: Optional[float] = None,
         **model_kwargs
-) -> np.ndarray:
-    raw_data, sample_rate = load_audio(file_path, tgt_len=tgt_len)
-    model = get_wav2vec(*model_args, **model_kwargs)
+    ) -> np.ndarray:
 
-    audio_features = ...
+    raw_data, sample_rate = load_audio(file_path, tgt_len=tgt_len)
+    model,processor = get_wav2vec()
+
+    #processing data
+    inputs = processor(raw_data, sampling_rate=sample_rate, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
+    audio_features = torch.squeeze(outputs.last_hidden_state).numpy()
+    #print(list(audio_features.shape))
 
     if pooling is not None:
         return pooling(audio_features, t_pooling)
