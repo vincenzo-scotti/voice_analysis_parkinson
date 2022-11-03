41d978f2c6bae026e0ab683c621d4775fd76bf12
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2022-11-03 17:53:28 +0100

    [CF] updated feature extractors files with adjustment

--- src/features/vggish.py
+++ src/features/vggish.py
@@ -1,7 +1,8 @@
 from typing import Optional
 import numpy as np
-from .utils import pooling, GlobalPooling
+from src.features.utils import pooling, GlobalPooling
 import torch
+import os
 
 vggish: Optional = None
 
@@ -10,6 +11,7 @@ def get_vggish():
     global vggish
     if vggish is None:
         vggish = torch.hub.load('harritaylor/torchvggish', 'vggish')
+        vggish.eval()
     return vggish
 
 
@@ -21,11 +23,23 @@ def get_vggish_features(
         **model_kwargs
 ) -> np.ndarray:
     model = get_vggish()
-    
-    #processing data
-    audio_features = model.forward(file_path)
 
+    #processing data
+    tensor_audio_features = model.forward(file_path)
+    audio_features = tensor_audio_features.detach().cpu().numpy()
     if pooling is not None:
         return pooling(audio_features, t_pooling)
     else:
         return audio_features
+
+'''
+FOR DEBUG PURPOSE
+if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    a = get_vggish()
+    a.eval()
+    res = a.forward("resources/data/Split_denoised_hindi/AUD-20210515-WA0002_000.wav")
+    print(type(res.detach().cpu().numpy()))
+
+    print(res.detach().cpu().numpy().shape)
+'''
--- src/features/wav2vec.py
+++ src/features/wav2vec.py
@@ -1,11 +1,12 @@
 from typing import Optional
 import numpy as np
 import torch
-from transformers import Wav2Vec2Processor, Wav2Vec2Model,Wav2Vec2CTCTokenizer, AutoFeatureExtractor
-from .utils import load_audio, pooling, GlobalPooling
+from transformers import Wav2Vec2Processor, Wav2Vec2Model, Wav2Vec2CTCTokenizer, AutoFeatureExtractor
+from src.features.utils import load_audio, pooling, GlobalPooling
 
 wav2vec: Optional = None
 processorWav2vec: Optional = None
+
 
 def get_wav2vec():
     global wav2vec
@@ -18,25 +19,38 @@ def get_wav2vec():
         processorWav2vec = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
     return wav2vec, processorWav2vec
 
+
 def get_wav2vec_features(
         file_path: str,
         *model_args,
         t_pooling: Optional[GlobalPooling] = None,
         tgt_len: Optional[float] = None,
         **model_kwargs
-    ) -> np.ndarray:
-
+) -> np.ndarray:
     raw_data, sample_rate = load_audio(file_path, tgt_len=tgt_len)
-    model,processor = get_wav2vec()
+    model, processor = get_wav2vec()
 
-    #processing data
+    # processing data
     inputs = processor(raw_data, sampling_rate=sample_rate, return_tensors="pt")
     with torch.no_grad():
         outputs = model(**inputs)
     audio_features = torch.squeeze(outputs.last_hidden_state).numpy()
-    #print(list(audio_features.shape))
+    # print(list(audio_features.shape))
 
     if pooling is not None:
         return pooling(audio_features, t_pooling)
     else:
         return audio_features
+
+
+'''
+FOR DEBUG PURPOSE
+if __name__ == "__main__":
+    mod,proc = get_wav2vec()
+    raw_data, sample_rate = load_audio("resources/data/Split_denoised_hindi/AUD-20210515-WA0002_000.wav", tgt_len=None)
+    inputs = proc(raw_data, sampling_rate=16000, return_tensors="pt")
+    with torch.no_grad():
+        outputs = mod(**inputs)
+    audio_features = torch.squeeze(outputs.last_hidden_state).numpy()
+    print(audio_features,audio_features.shape)
+'''
