d8b30ba32669286cb948bcfed3d7c8eee87e7609
ClaudioFerrante95 <claudio.ferrante95@gmail.com>
2022-11-23 17:16:44 +0100

    [CF] fix on trunc function

--- src/features/utils.py
+++ src/features/utils.py
@@ -26,21 +26,15 @@ def trunc_audio(x, y, d, chunk_len=4.0) -> List[Tuple[np.ndarray,np.ndarray]]:
     windows_number = int(math.ceil(d / chunk_len))
     #durata totale : #campioni_totali = durata chunk  : #campioni_chunk
     sample_number = x.shape[0]
-    chunk_sample_number = int((chunk_len * sample_number) // d)
+    chunk_sample_number = int(((chunk_len * sample_number) // d))
     # 'median' o 'edge' (entrambe le direzioni)
 
     #split matrix of features, chunk starts from 0
+    diff = chunk_sample_number - (sample_number % chunk_sample_number)
+    x=np.pad(x, ((diff // 2, math.ceil(diff / 2)), (0, 0)), 'edge')
     for chunk in range(0,windows_number):
         start_x = chunk * chunk_sample_number
-        if chunk == windows_number-1:
-            #padding part
-            last_chunk = x[start_x:, :]
-            diff =chunk_sample_number-(sample_number % chunk_sample_number)
-            #TODO check if .T is needed
-            last_chunk = np.pad(last_chunk, ((diff//2, math.ceil(diff/2)),(0,0)),'edge')
-            list_chunks.append(last_chunk)
-        else:
-            list_chunks.append(x[start_x:start_x+chunk_sample_number,:])
+        list_chunks.append(x[start_x:start_x+chunk_sample_number,:])
 
     final_list_chunks = [(elem, y) for elem in list_chunks]
     return final_list_chunks
