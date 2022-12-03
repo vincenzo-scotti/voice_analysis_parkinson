41559f9e851fc479009ee85b87d7a8260b45def5
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-12-03 12:49:57 +0100

    Complete main.py script with detailed metrics computation.

--- src/bin/main.py
+++ src/bin/main.py
@@ -22,14 +22,21 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix
+from sklearn.metrics import (
+    classification_report,
+    accuracy_score,
+    precision_recall_fscore_support,
+    roc_auc_score,
+    roc_curve,
+    precision_recall_curve,
+    confusion_matrix
+)
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
+from adapt.feature_based import CORAL
 
 from features import GlobalPooling, pooling, FEATURE_EXTRACTORS, trunc_audio
-
-from adapt.feature_based import CORAL
 
 
 def main(args: Namespace):
@@ -40,7 +47,7 @@ def main(args: Namespace):
     with open(args.configs_file_path) as f:
         configs: Dict = yaml.full_load(f)
     # Create experiment directory
-    current_experiment_dir_path = os.path.join(args.experiments_dir_path, 'experiment_' + date_time_experiment)
+    current_experiment_dir_path = os.path.join(configs['experiments_dir'], 'experiment_' + date_time_experiment)
     os.mkdir(current_experiment_dir_path)
     # Dump configs
     copy2(args.configs_file_path, os.path.join(current_experiment_dir_path, 'configs.yaml'))
@@ -48,13 +55,19 @@ def main(args: Namespace):
     output_file_path = os.path.join(current_experiment_dir_path, 'results.txt')
     f = open(output_file_path, "x")
     f.close()
+    # Scores data path
+    scores_file_path = os.path.join(current_experiment_dir_path, 'scores.csv')
+    # Scores Data Frame columns
+    score_cols = ['feature', 'pooling', 'adaptation', 'data_set', 'metric', 'value']
+    # Accumulator for scores data
+    score_data: List[Tuple[str, str, bool, str, str, Union[float, List[float], int, List[List[int]]]]] = []
     # Label encoder
     le = LabelEncoder()
 
     # Load source language audio paths and labels
-    src_language_metadata_df = pd.read_csv(os.path.join(args.source_language_data_dir_path, 'metadata.csv')).sample(5, random_state=1)
+    src_language_metadata_df = pd.read_csv(os.path.join(configs['src_lang_dir'], 'metadata.csv'))
     X_src_paths: List[str] = [
-        os.path.join(args.source_language_data_dir_path, file_name) for file_name in src_language_metadata_df.file_name
+        os.path.join(configs['src_lang_dir'], file_name) for file_name in src_language_metadata_df.file_name
     ]
     X_src_idxs: np.ndarray = np.arange(len(X_src_paths))
     y_src_labels: np.ndarray = le.fit_transform([label for label in src_language_metadata_df.label])
@@ -64,10 +77,9 @@ def main(args: Namespace):
     )
 
     # Load target language audio paths and labels
-    #TODO remove sample
-    tgt_language_metadata_df = pd.read_csv(os.path.join(args.target_language_data_dir_path, 'metadata.csv')).sample(5, random_state=1)
+    tgt_language_metadata_df = pd.read_csv(os.path.join(configs['tgt_lang_dir'], 'metadata.csv'))
     X_tgt_paths: List[str] = [
-        os.path.join(args.target_language_data_dir_path, file_name) for file_name in tgt_language_metadata_df.file_name
+        os.path.join(configs['tgt_lang_dir'], file_name) for file_name in tgt_language_metadata_df.file_name
     ]
     y_tgt_labels: np.ndarray = le.transform([label for label in tgt_language_metadata_df.label])
 
@@ -132,7 +144,6 @@ def main(args: Namespace):
             # Do PCA (using already fit model from source language)
             X_tgt: np.ndarray = pca.transform(X_tgt)
 
-
             for do_adaptation in [False, True]:
                 # Train classifier (on source data)
                 cls: SVC = SVC(probability=True)
@@ -146,22 +157,38 @@ def main(args: Namespace):
                 cls.fit(X_src_train_adapted, y_src_train_split)
                 # Test classifier (on source data)
                 y_src_test_pred = cls.predict(X_src_test_adapted)
+                y_src_test_proba = cls.predict_proba(X_src_test_adapted)[:, 1]
+
                 src_cls_report = classification_report(y_src_test_split, y_src_test_pred)
-                y_src_test_proba = cls.predict_proba(X_src_test_adapted)
-                src_auc_score = roc_auc_score(y_src_test_split, y_src_test_proba[:,1])
+                src_accuracy_score = accuracy_score(y_src_test_split, y_src_test_pred)
+                src_precision_score, src_recall_score, src_fscore, src_support = precision_recall_fscore_support(
+                    y_src_test_split, y_src_test_pred, average='macro'
+                )
+                src_specificity_score = recall_score(y_src_test_split, y_src_test_pred, pos_label=0)
+                src_auc_score = roc_auc_score(y_src_test_split, y_src_test_proba)
+                src_fpr, src_tpr, src_roc_threshold = roc_curve(y_src_test_split, y_src_test_proba)
+                src_pr, src_rc, src_pr_rc_threshold = precision_recall_curve(y_src_test_split, y_src_test_proba)
                 src_confusion_matrix = confusion_matrix(y_src_test_split, y_src_test_pred)
 
                 # Test classifier (on target data)
                 y_tgt_pred = cls.predict(X_tgt)
-                tgt_cls_report = classification_report(y_tgt_labels_split, y_tgt_pred)
-                y_tgt_proba = cls.predict_proba(X_tgt)
-                tgt_auc_score = roc_auc_score(y_tgt_labels_split, y_tgt_proba[:,1])
-                tgt_confusion_matrix = confusion_matrix(y_tgt_labels_split, y_tgt_pred)
+                y_tgt_proba = cls.predict_proba(X_tgt)[:, 1]
 
+                tgt_cls_report = classification_report(y_tgt_labels_split, y_tgt_pred)
+                tgt_accuracy_score = accuracy_score(y_tgt_labels_split, y_tgt_pred)
+                tgt_precision_score, tgt_recall_score, tgt_fscore, tgt_support = precision_recall_fscore_support(
+                    y_tgt_labels_split, y_tgt_pred, average='macro'
+                )
+                tgt_specificity_score = recall_score(y_tgt_labels_split, y_tgt_pred, pos_label=0)
+                tgt_auc_score = roc_auc_score(y_tgt_labels_split, y_tgt_proba[:, 1])
+                tgt_fpr, tgt_tpr, tgt_roc_threshold = roc_curve(y_tgt_labels_split, y_tgt_proba)
+                tgt_pr, tgt_rc, tgt_pr_rc_threshold = precision_recall_curve(y_tgt_labels_split, y_tgt_proba)
+
+                tgt_confusion_matrix = confusion_matrix(y_tgt_labels_split, y_tgt_pred)
 
                 # Log results
                 with open(output_file_path, 'a') as f:
-                    print(f"Feature type: {feature}, pooling type: {t_pooling}, domain adaptation: {do_adaptation}\n", file=f)
+                    print(f"Feature type: {feature}, pooling type: {t_pooling.value}, domain adaptation: {do_adaptation}\n", file=f)
                     print("Source language classification results\n", file=f)
                     print(f"Classification report: \n{src_cls_report}\n", file=f)
                     print(f"ROC AUC score: {src_auc_score}\n", file=f)
@@ -172,30 +199,54 @@ def main(args: Namespace):
                     print(f"ROC AUC score: {tgt_auc_score}\n", file=f)
                     print(f"Confusion matrix (true label on row, predicted on columns): \n{tgt_confusion_matrix}\n", file=f)
                     print("\n\n\n\n")
+                # Append scores
+                score_data += [
+                    (feature, t_pooling.value, do_adaptation, 'src', 'accuracy', src_accuracy_score),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'precision', src_precision_score),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'recall', src_recall_score),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'fscore', src_fscore),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'support', src_support),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'specificity', src_specificity_score),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'roc_auc', src_auc_score),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'fpr', src_fpr.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'tpr', src_tpr.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'roc_thresholds', src_roc_threshold.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'precisions', src_pr.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'recalls', src_rc.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'pr_rc_thresholds', src_pr_rc_threshold.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'src', 'confusion_matrix', src_confusion_matrix.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'accuracy', tgt_accuracy_score),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'precision', tgt_precision_score),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'recall', tgt_recall_score),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'fscore', tgt_fscore),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'support', tgt_support),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'specificity', tgt_specificity_score),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'roc_auc', tgt_auc_score),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'fpr', tgt_fpr.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'tpr', tgt_tpr.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'roc_thresholds', tgt_roc_threshold.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'precisions', tgt_pr.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'recalls', tgt_rc.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'pr_rc_thresholds', tgt_pr_rc_threshold.tolist()),
+                    (feature, t_pooling.value, do_adaptation, 'tgt', 'confusion_matrix', tgt_confusion_matrix.tolist())
+                ]
+    # Create data frame with measured scores
+    df: pd.DataFrame = pd.DataFrame(score_data, columns=score_cols)
+    # Save data frame
+    df.to_csv(scores_file_path, index=False)
 
     return 0
 
 
 if __name__ == "__main__":
-    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    # Disable CUDA devices (use only CPU)
+    os.environ['CUDA_VISIBLE_DEVICES'] = ''
     # Instantiate argument parser
     args_parser: ArgumentParser = ArgumentParser()
     # Add arguments to parser
     args_parser.add_argument(
         '--configs_file_path', type=str, default='./resources/configs/configs.yaml',
         help="Path to the YAML file with the configuration."
-    )
-    args_parser.add_argument(
-        '--source_language_data_dir_path', type=str, default='./resources/data/Split_denoised_english/',
-        help="Path to the directory with the audio clips in the source language for the experiments."
-    )
-    args_parser.add_argument(
-        '--target_language_data_dir_path', type=str, default='./resources/data/Split_denoised_hindi/',
-        help="Path to the directory with the audio clips in the source language for the experiments."
-    )
-    args_parser.add_argument(
-        '--experiments_dir_path', type=str, default='./experiments/',
-        help="Path to the directory to store the results of the experiments."
     )
     # Run experiment
     main(args_parser.parse_args(sys.argv[1:]))
